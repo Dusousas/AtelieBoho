66be2782419c7e8d8bf4c9a3ad5284830fd78b19
dusousas <dusousadeveloper@gmail.com>
2024-09-30 13:09:34 -0300

    bugs

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import Marquee from 'react-marquee-slider';
 
-export default function Brands() {
-  const logos = [
-    { src: '/Redken.png', alt: 'Redken' },
-    { src: '/Loreal1.png', alt: 'Loreal' },
-    { src: '/Wella1.png', alt: 'Wella' },
-    { src: '/Kerastase1.png', alt: 'Kerastase' },
-    { src: '/Olorchee1.png', alt: 'Olorchee' },
-  ];
+const logos = [
+  { src: '/Redken.png', alt: 'Redken' },
+  { src: '/Loreal1.png', alt: 'Loreal' },
+  { src: '/Wella1.png', alt: 'Wella' },
+  { src: '/Kerastase1.png', alt: 'Kerastase' },
+  { src: '/Olorchee1.png', alt: 'Olorchee' },
+];
+
+const Brands: React.FC = () => {
+  const handleInit = () => {
+    console.log('Marquee initialized');
+  };
+
+  const handleFinish = () => {
+    console.log('Marquee finished');
+  };
 
   return (
     <section id="services" className="bg-[#F9ECE1] customH py-4 overflow-hidden">
       <div className="maxWidth">
-        <Marquee velocity={20} scatterRandomly={false} resetAfterTries={100}>
+        <Marquee
+          velocity={30}
+          scatterRandomly={false}
+          resetAfterTries={100}
+          direction="rtl" // Direção do marquee
+          onInit={handleInit} // Callback na inicialização
+          onFinish={handleFinish} // Callback ao finalizar
+        >
           {logos.map((logo, index) => (
             <div key={index} className="mx-32">
               <img className="w-[110px]" src={logo.src} alt={logo.alt} />
@@ -23,4 +38,6 @@ export default function Brands() {
       </div>
     </section>
   );
-}
+};
+
+export default Brands;
